93e93551277636e8c9e61302c21077df9f025281
Zach Silver <zach.silver@gradientboostedinvestments.com>
2024-07-25 21:18:25 -0400

    .

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ln -sv $DIR/tmux.conf ~/.tmux.conf
 ln -sv $DIR/zshrc.sh ~/.zshrc
 ln -sv $DIR/vterm.sh ~/.vterm.sh
-cp $DIR/rclone.conf ~/.config/rclone/rclone.conf
 
 read -p "Do you want to install work specific settings (work_gitconfig, workrc, etc.)? (y/n)>" -r
 
